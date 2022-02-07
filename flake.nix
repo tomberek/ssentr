49af8d4047c5cb0fb3a6d274e92f7a8eddc1e88c
Tom Bereknyei <tomberek@gmail.com>
2022-02-07 13:21:21 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Send SSE when files change";
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.ssentr = nixpkgs.legacyPackages.x86_64-linux.buildGoModule {
+      name = "ssentr";
+      src = ./.;
+      vendorSha256 = "sha256-L1TzJ6gAbMRVuALsGSRioXU6eQdMCHlbkEmeR4qz4Lg=";
+    };
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ssentr;
+    hydraJobs.ssentr.x86_64-linux = self.defaultPackage.x86_64-linux;
+  };
+}
