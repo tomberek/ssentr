527c21434639d08c57ec06e08274f30bb3f41178
Tom Bereknyei <tomberek@gmail.com>
2022-02-09 08:21:46 -0500

    add meta and make a proper app for update go hash

--- flake.nix
+++ flake.nix
@@ -6,8 +6,23 @@
       name = "ssentr";
       src = self;
       vendorSha256 = "sha256-L1TzJ6gAbMRVuALsGSRioXU6eQdMCHlbkEmeR4qz4Lg=";
+      meta = with nixpkgs.legacyPackage.x86_64-linux.lib; {
+        maintainers = [ maintainers.tomberek ];
+        platforms = platforms.linux;
+        license = licenses.mit;
+      };
     };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.ssentr;
+
+    apps.x86_64-linux.update = {
+      type = "app";
+      program = (with nixpkgs.legacyPackages.x86_64-linux; writeScript "update.sh" ''
+        ${nix-prefetch}/bin/nix-prefetch --file 'fetchTarball "channel:nixos-unstable"' \
+          "{ sha256 }: (builtins.getFlake \"$PWD\").defaultPackage.x86_64-linux.go-modules.overrideAttrs
+                (_: { modSha256 = sha256; })" \
+          --option experimental-features 'nix-command flakes'
+      '').outPath;
+      };
     hydraJobs.ssentr.x86_64-linux = self.defaultPackage.x86_64-linux;
   };
 }
