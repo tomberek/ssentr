6da8a1721226726aca49560479ea111596c7c252
Tom Bereknyei <tomberek@gmail.com>
2022-12-05 02:12:20 -0500

    fix: expose blueprint

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
   description = "Send SSE when files change";
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.ssentr = nixpkgs.legacyPackages.x86_64-linux.buildGo118Module {
-      name = "ssentr";
-      src = self;
-      vendorSha256 = "sha256-3d5iPPz6iccXq1kJyp6IgyQBGlKI0yZUKZIedeuDzz8=";
-      meta = with nixpkgs.legacyPackages.x86_64-linux.lib; {
-        maintainers = [ maintainers.tomberek ];
-        platforms = platforms.linux;
-        license = licenses.mit;
+  outputs = {
+    self,
+    nixpkgs,
+  }: rec {
+    packages.x86_64-linux.ssentr = nixpkgs.legacyPackages.x86_64-linux.callPackage blueprints.ssentr {src = self;};
+    blueprints.ssentr = {
+      src,
+      lib,
+      buildGo118Module,
+    }:
+      buildGo118Module {
+        name = "ssentr";
+        src = src;
+        vendorSha256 = "sha256-3d5iPPz6iccXq1kJyp6IgyQBGlKI0yZUKZIedeuDzz8=";
+        meta = with lib; {
+          maintainers = [maintainers.tomberek];
+          platforms = platforms.linux;
+          license = licenses.mit;
+        };
       };
-    };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.ssentr;
 
     apps.x86_64-linux.update = {
       type = "app";
-      program = (with nixpkgs.legacyPackages.x86_64-linux; writeScript "update.sh" ''
-        ${nix-prefetch}/bin/nix-prefetch --file 'fetchTarball "channel:nixos-unstable"' \
-          "{ sha256 }: (builtins.getFlake \"$PWD\").defaultPackage.x86_64-linux.go-modules.overrideAttrs
-                (_: { modSha256 = sha256; })" \
-          --option experimental-features 'nix-command flakes'
-      '').outPath;
-      };
+      program =
+        (with nixpkgs.legacyPackages.x86_64-linux;
+          writeScript "update.sh" ''
+            ${nix-prefetch}/bin/nix-prefetch --file 'fetchTarball "channel:nixos-unstable"' \
+              "{ sha256 }: (builtins.getFlake \"$PWD\").defaultPackage.x86_64-linux.go-modules.overrideAttrs
+                    (_: { modSha256 = sha256; })" \
+              --option experimental-features 'nix-command flakes'
+          '')
+        .outPath;
+    };
     hydraJobs.ssentr.x86_64-linux = self.defaultPackage.x86_64-linux;
   };
 }
