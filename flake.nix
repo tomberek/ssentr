306a73e5e9a006b84abfad6617f6cba2a6ef0835
Tom Bereknyei <tomberek@gmail.com>
2023-05-30 09:02:34 -0400

    fix: bump actions, refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,63 @@
 {
   description = "Send SSE when files change";
+
   outputs = {
     self,
     nixpkgs,
-  }: rec {
-    packages.x86_64-linux.ssentr = nixpkgs.legacyPackages.x86_64-linux.callPackage blueprints.ssentr {};
+  }: let
+    systems = {
+      "x86_64-linux" = {};
+      "x86_64-darwin" = {};
+      "aarch64-linux" = {};
+      "aarch64-darwin" = {};
+    };
+    generate = arg: builtins.mapAttrs arg (builtins.intersectAttrs systems nixpkgs.legacyPackages);
+  in rec {
+    packages = generate (system: pkgs: rec {
+      ssentr = pkgs.callPackage blueprints.ssentr {};
+      default = ssentr;
+      ssentr-baked = builtins.fetchClosure {
+        fromPath = "/nix/store/sazl2bwfrj9sm5qm6qxiqag5ghmd24dp-ssentr";
+        fromStore = "https://cache.floxdev.com";
+      };
+    });
+
     blueprints.ssentr = {
-      src ? self,
       lib,
       buildGo118Module,
     }:
       buildGo118Module {
         name = "ssentr";
+
+        # Ignore nix file changes during build
         src = builtins.path {
-          path = ./.;
+          filter = path: type: ! builtins.elem (builtins.baseNameOf path) ["flake.nix" "flake.lock"];
+          path = self;
           name = "ssentr";
         };
         vendorSha256 = "sha256-3d5iPPz6iccXq1kJyp6IgyQBGlKI0yZUKZIedeuDzz8=";
         meta = with lib; {
           maintainers = [maintainers.tomberek];
-          platforms = platforms.linux;
+          platforms = builtins.attrNames systems;
           license = licenses.mit;
         };
-     };
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ssentr;
+      };
 
-    apps.x86_64-linux.update = {
-      type = "app";
-      program =
-        (with nixpkgs.legacyPackages.x86_64-linux;
-          writeScript "update.sh" ''
-            ${nix-prefetch}/bin/nix-prefetch --file 'fetchTarball "channel:nixos-unstable"' \
-              "{ sha256 }: (builtins.getFlake \"$PWD\").defaultPackage.x86_64-linux.go-modules.overrideAttrs
-                    (_: { modSha256 = sha256; })" \
-              --option experimental-features 'nix-command flakes'
-          '')
-        .outPath;
-    };
-    hydraJobs.ssentr.x86_64-linux = self.defaultPackage.x86_64-linux;
+    apps = generate (system: pkgs: {
+      update = {
+        type = "app";
+        program =
+          (with pkgs;
+            writeScript "update.sh" ''
+              ${nix-prefetch}/bin/nix-prefetch --file 'fetchTarball "channel:nixos-unstable"' \
+                "{ sha256 }: (builtins.getFlake \"$PWD\").defaultPackage.x86_64-linux.go-modules.overrideAttrs
+                      (_: { modSha256 = sha256; })" \
+                --option experimental-features 'nix-command flakes'
+            '')
+          .outPath;
+      };
+    });
+
+    hydraJobs = generate (system: pkgs: {ssentr = self.packages.${system}.ssentr;});
   };
 }
