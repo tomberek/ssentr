36041f9f46f843c83644c2fe07a614cb294b72ce
Tom Bereknyei <tomberek@gmail.com>
2023-05-31 20:50:31 -0400

    fix: conditional fetchClosure

--- flake.nix
+++ flake.nix
@@ -13,14 +13,23 @@
     };
     generate = arg: builtins.mapAttrs arg (builtins.intersectAttrs systems nixpkgs.legacyPackages);
   in rec {
-    packages = generate (system: pkgs: rec {
-      ssentr = pkgs.callPackage blueprints.ssentr {};
-      default = ssentr;
-      ssentr-baked = builtins.fetchClosure {
-        fromPath = "/nix/store/sazl2bwfrj9sm5qm6qxiqag5ghmd24dp-ssentr";
-        fromStore = "https://cache.floxdev.com";
-      };
-    });
+    packages = generate (
+      system: pkgs:
+        rec {
+          ssentr = pkgs.callPackage blueprints.ssentr {};
+          default = ssentr;
+        }
+        // (
+          if builtins ? fetchClosure
+          then {
+            ssentr-baked = builtins.fetchClosure {
+              fromPath = "/nix/store/sazl2bwfrj9sm5qm6qxiqag5ghmd24dp-ssentr";
+              fromStore = "https://cache.floxdev.com";
+            };
+          }
+          else {}
+        )
+    );
 
     blueprints.ssentr = {
       lib,
